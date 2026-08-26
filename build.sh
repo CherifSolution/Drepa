aaf202c0691826360cc5f3bf9982f68bb9322be9
Cherif Chabi <cherif.chabi@digiglesolutions.com>
2026-08-26 16:49:33 +0100

    Fix index.html avec flutter_bootstrap.js

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 # Télécharger le SDK Flutter stable
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 
-# Ajouter Flutter au PATH temporaire
+# Ajouter Flutter au PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Activer le support Web et récupérer les dépendances
+# Activer le Web et récupérer les dépendances
 flutter config --enable-web
 flutter pub get
 
-# Compiler pour le web
-flutter build web --release
+# Compiler pour le Web en HTML
+flutter build web --release --no-wasm
