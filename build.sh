eb5710f9f88c077149d1c9031cb35cb1e96d619e
Cherif Chabi <cherif.chabi@digiglesolutions.com>
2026-08-26 16:55:05 +0100

    Fix build.sh renderer et initialisation flutter.js

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 # Ajouter Flutter au PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Activer le Web et récupérer les dépendances
+# Activer le Web et installer les dépendances
 flutter config --enable-web
 flutter pub get
 
-# Compiler pour le Web en HTML
-flutter build web --release --no-wasm
+# Compiler pour le Web (HTML renderer)
+flutter build web --release --web-renderer html
